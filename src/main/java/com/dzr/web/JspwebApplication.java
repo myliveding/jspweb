8d81321c34f65abd27a23b2dfac1bfc028abe911
dingzr <dingzr@joyowo.com>
2017-11-30 10:26:25 +0800

    Initial commit

---
+++ src/main/java/com/dzr/web/JspwebApplication.java
@@ -0,0 +1,12 @@
+package com.dzr.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JspwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JspwebApplication.class, args);
+	}
+}
